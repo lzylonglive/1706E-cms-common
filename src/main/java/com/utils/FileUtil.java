ed8c85f76b9e19b2b56373fc3e9799d8397ff5a6
lzylonglive <691369220@QQ.com>
2019-12-19 08:58:09 +0800

    工具类

--- src/main/java/com/utils/FileUtil.java
+++ src/main/java/com/utils/FileUtil.java
@@ -126,4 +126,40 @@ public class FileUtil {
 		return list;
 	}
 	
+	/**
+	 * 获取文件夹下的文件列表
+	 * @param pathName
+	 * @return
+	 */
+	public static List<String> getFileList(String pathName) {
+		String[] list = new File(pathName).list();
+		List<String> fileList = new ArrayList<String>();
+		for (String string : list) {
+			File subFile = new File(pathName + "\\" + string);
+			if (subFile != null && subFile.exists() && subFile.isFile())
+				fileList.add(pathName + "\\" + string);
+		}
+		return fileList;
+
+	}
+	
+	/**
+	 * 读取文件内容
+	 * 
+	 * @throws IOException
+	 */
+//	public static String readFileToString(String fileName) throws IOException {
+//
+//		StringBuilder sb = new StringBuilder();
+//
+//		File file = new File(fileName);
+//		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+//		String lineString = null;
+//
+//		while ((lineString = bufferedReader.readLine()) != null) {
+//			sb.append(lineString).append("\n");
+//		}
+//		return sb.toString();
+//	}
+	
 }
